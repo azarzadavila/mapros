52effbb16f9350c004b11da72a6ac3bb78fea820
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-09-15 11:29:19 +0200

    Set env for doc

--- mapros/urls.py
+++ mapros/urls.py
@@ -13,11 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 from mapros import views
 
+# From https://drf-yasg.readthedocs.io/en/stable/readme.html
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Doc", default_version="v1", description="API Doc Description",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    # -- From https://drf-yasg.readthedocs.io/en/stable/readme.html -- #
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+    # -- #
     path("hello/", views.hello_world),
     path("auth/", views.signin),
     path("admin/", admin.site.urls),
