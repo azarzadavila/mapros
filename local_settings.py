cb40d5f098e98b18af55ada6186d40ff4830341e
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-09-30 11:01:25 +0200

    CORS install

--- local_settings.py
+++ local_settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "drf_yasg",
     "authentification.apps.AuthentificationConfig",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -58,6 +60,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "mapros.urls"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 TEMPLATES = [
     {
