70f4168c95b54fd25c124d18cc05856790c717b7
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-10-12 18:05:18 +0200

    Print proof

--- formal/proof.py
+++ formal/proof.py
@@ -1,5 +1,23 @@
+def print_tab(s, tab):
+    for i in range(tab):
+        print("\t", end="")
+    print(s, end="")
+
+
 class Proof:
-    def __init__(self, statement, children, statement_proof):
+    def __init__(
+        self, statement, children, statement_proof, parent=None, parent_id=None
+    ):
         self.statement = statement
         self.children = children
         self.statement_proof = statement_proof
+        self.parent = parent
+        self.parent_id = parent_id
+
+    def print(self, tab=0):
+        if self.parent_id is not None:
+            print_tab(self.parent_id, tab)
+        print(self.statement)
+        for child in self.children:
+            child.print(tab + 1)
+        print("Proof : ", self.statement_proof)
