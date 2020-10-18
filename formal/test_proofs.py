bb5d12994ac5a6848e55508331b66064b8032e11
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-10-18 17:22:38 +0200

    Simple formal proof test

--- formal/test_proofs.py
+++ formal/test_proofs.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 
-from formal.proof import check_scope
+from formal.grammar import PredicateConstant, BinaryConnector, Sentence
+from formal.proof import check_scope, Proof
+from formal.rules_inference import StatementProof
+
+BC = BinaryConnector
 
 
 class SimpleUnits(TestCase):
@@ -14,3 +18,16 @@ class SimpleUnits(TestCase):
         self.assertFalse(check_scope((1, 1), (2,)))
         self.assertFalse(check_scope((2, 1, 1), (2, 2,)))
         self.assertTrue(check_scope((2, 1, 1), (2, 1,)))
+
+
+class CompleteFirstOrderProof(TestCase):
+    def test_simple(self):
+        A = PredicateConstant("A")
+        B = PredicateConstant("B")
+        proof = Proof(Sentence(A, BC.CONJUCTION, B), [], None)
+        proof.add_statement_child(A, ())
+        proof.add_statement_child(B, ())
+        proof.edit_statement_proof(StatementProof("premise", []), (0,))
+        proof.edit_statement_proof(StatementProof("premise", []), (1,))
+        proof.edit_statement_proof(StatementProof("adjunction", [(0,), (1,)]), ())
+        self.assertTrue(proof.check_proof(()))
