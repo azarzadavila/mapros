60878f651815f12cf82c200a9a8066dec20c5d92
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-10-28 10:49:23 +0100

    Refactor grammar

--- formal/grammar.py
+++ formal/grammar.py
@@ -1,158 +1,87 @@
+import abc
 from enum import Enum, auto
 
 
-class Sentence:
-    def __init__(self, *data):
-        self.data = data
-        if not check_sentence(self):
-            raise ValueError("This is not a correct sentence.")
-
-    def __str__(self):
-        s = "("
-        for i in range(len(self.data) - 1):
-            s += str(self.data[i])
-            s += " "
-        s += str(self.data[-1])
-        s += ")"
-        return s
-
-    def substitute(self, var, new_var):
-        if len(self.data) == 2:
-            return Sentence(NEGATION, self.data[1].substitute(var, new_var))
-        if len(self.data) == 3:
-            if isinstance(self.data[1], BinaryConnector):
-                return Sentence(
-                    self.data[0].substitute(var, new_var),
-                    self.data[1],
-                    self.data[2].substitute(var, new_var),
-                )
-            elif self.data[1] == var:
-                return self
-            else:
-                if self.data[1] == new_var:
-                    print(
-                        "Warning : there already exists a quantifier associated with the new variable, variable capture might happen"
-                    )
-                return Sentence(
-                    self.data[0], self.data[1], self.data[2].substitute(var, new_var)
-                )
-
-    def __eq__(self, other):
-        return self.data == other.data
+class Sentence(metaclass=abc.ABCMeta):
+    pass
 
 
-class IDSentence:
-    def __init__(self, id):
-        if id is None:
-            raise ValueError("The id of an IDSentence may not be None.")
-        self.id = id
-
-    def __str__(self):
-        return str(self.id)
-
-    def __eq__(self, other):
-        return other.id == self.id
-
-    def substitute(self, var, new_var):
-        # TODO
-        pass
+class ConstantPredicate(Sentence):
+    def __init__(self, symbol):
+        self.symbol = symbol
 
 
-def str_with_arity(arity_obj, args):
-    s = str(arity_obj)
-    s += "("
-    for i in range(len(args) - 1):
-        s += str(args[i])
-        s += ", "
-    s += str(args[-1])
-    s += ")"
-    return s
+class Predicate(Sentence):
+    def __init__(self, symbol, *terms):
+        self.symbol = symbol
+        self.terms = list(terms)
+        for term in self.terms:
+            if not isinstance(term, Term):
+                raise ValueError("incorrect term")
 
 
-class AtomicSentence:
-    def __init__(self, data):
-        self.data = data
-        if not check_atomic_sentence(self):
-            raise ValueError("This is not a correct atomic sentence.")
-
-    def __eq__(self, other):
-        return self.data == other.data
-
-    def __str__(self):
-        if len(self.data) == 1:
-            return str(self.data[0])
-        return str_with_arity(self.data[0], self.data[1])
-
-    def substitute(self, var, new_var):
-        return AtomicSentence(
-            (self.data[0], tuple(x.substitute(var, new_var) for x in self.data[1]))
-        )
+class UnaryConnectorSentence(Sentence):
+    def __init__(self, connector, sentence):
+        self.connector = connector
+        self.sentence = sentence
+        if not isinstance(connector, UnaryConnector):
+            raise ValueError("incorrect connector")
+        if not is_sentence(sentence):
+            raise ValueError("incorrect sentence")
 
 
-class Term:
-    def __init__(self, *data):
-        self.data = data
-        if not check_term(self):
-            raise ValueError("This is not a correct term.")
-
-    def __eq__(self, other):
-        return self.data == other.data
-
-    def __str__(self):
-        if len(self.data) == 1:
-            return str(self.data[0])
-        return str_with_arity(self.data[0], self.data[1])
-
-    def substitute(self, var, new_var):
-        return Term(
-            self.data[0], tuple(x.substitute(var, new_var) for x in self.data[1])
-        )
+class BinaryConnectorSentence(Sentence):
+    def __init__(self, connector, sentence1, sentence2):
+        self.connector = connector
+        self.sentence1 = sentence1
+        self.sentence2 = sentence2
+        if not isinstance(connector, BinaryConnector):
+            raise ValueError("incorrect connector")
+        if not is_sentence(sentence1) or not is_sentence(sentence2):
+            raise ValueError("incorrect sentence")
 
 
-class Predicate:
-    def __init__(self, arity, name=None):
-        self.arity = arity
-        self.name = name
-        if not isinstance(arity, int) or arity < 0:
-            raise ValueError("The arity of a predicate can not be negative.")
-
-    def __eq__(self, other):
-        return self.arity == other.arity and self.name == other.name
-
-    def __str__(self):
-        return self.name
-
-
-class LogicFunction:
-    def __init__(self, arity, name=None):
-        self.arity = arity
-        self.name = name
-        if not isinstance(arity, int) or arity < 0:
-            raise ValueError("The arity of a function can not be negative.")
-
-    def __eq__(self, other):
-        return self.arity == other.arity and self.name == other.name
-
-    def __str__(self):
-        return self.name
+class QuantifierSentence(Sentence):
+    def __init__(self, quantifier, var, sentence):
+        self.quantifier = quantifier
+        self.var = var
+        self.sentence = sentence
+        if not isinstance(quantifier, Quantifier):
+            raise ValueError("incorrect quantifier")
+        if not isinstance(var, Variable):
+            raise ValueError("incorrect variable")
+        if not is_sentence(sentence):
+            raise ValueError("incorrect sentence")
 
 
-class __Negation(Enum):
+class Term(metaclass=abc.ABCMeta):
+    pass
+
+
+class Constant(Term):
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+
+class Variable(Term):
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+
+class Function(Term):
+    def __init__(self, symbol, *terms):
+        self.symbol = symbol
+        self.terms = list(terms)
+        for term in self.terms:
+            if not isinstance(term, Term):
+                raise ValueError("incorrect term")
+
+
+class UnaryConnector(Enum):
     NEGATION = auto()
 
     def __str__(self):
-        return "NOT"
-
-
-NEGATION = __Negation.NEGATION
-
-
-class Quantifier(Enum):
-    UNIVERSAL = auto()
-    EXISTENTIAL = auto()
-
-    def __str__(self):
-        smap = {self.UNIVERSAL: "FORALL", self.EXISTENTIAL: "EXISTS"}
+        smap = {self.NEGATION: "NOT"}
         return smap[self]
 
 
@@ -172,108 +101,14 @@ class BinaryConnector(Enum):
         return smap[self]
 
 
-class Constant:
-    def __init__(self, name):
-        self.name = name
+class Quantifier(Enum):
+    UNIVERSAL = auto()
+    EXISTENTIAL = auto()
 
     def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def substitute(self, var, new_var):
-        return self
+        smap = {self.UNIVERSAL: "FORALL", self.EXISTENTIAL: "EXISTS"}
+        return smap[self]
 
 
-class PredicateConstant:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-
-class Variable:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def substitute(self, var, new_var):
-        if self == var:
-            return new_var
-        else:
-            return self
-
-
-def check_binary_connector(x):
-    return isinstance(x, BinaryConnector)
-
-
-def check_terms(arity, args):
-    if len(args) != arity:
-        return False
-    for x in args:
-        if not check_term(x):
-            return False
-    return True
-
-
-def check_term(x):
-    if isinstance(x, Constant):
-        return True
-    if isinstance(x, Variable):
-        return True
-    if not isinstance(x, Term):
-        return False
-    if len(x.data) == 2:
-        fct = x.data[0]
-        if not isinstance(fct, LogicFunction):
-            return False
-        args = x.data[1]
-        return check_terms(fct.arity, args)
-    return False
-
-
-def check_atomic_sentence(x):
-    if isinstance(x, bool):
-        return True
-    if isinstance(x, PredicateConstant):
-        return True
-    if not isinstance(x, AtomicSentence):
-        return False
-    if len(x.data) == 2:
-        pred = x.data[0]
-        if not isinstance(pred, Predicate):
-            return False
-        args = x.data[1]
-        return check_terms(pred.arity, args)
-    return False
-
-
-def check_sentence(x):
-    if isinstance(x, IDSentence):
-        return True
-    if check_atomic_sentence(x):
-        return True
-    if not isinstance(x, Sentence):
-        return False
-    if len(x.data) == 2:
-        return x.data[0] == NEGATION and check_sentence(x.data[1])
-    if len(x.data) == 3:
-        if isinstance(x.data[0], Quantifier):
-            return isinstance(x.data[1], Variable) and check_sentence(x.data[2])
-        return (
-            check_sentence(x.data[0])
-            and isinstance(x.data[1], BinaryConnector)
-            and check_sentence(x.data[2])
-        )
-    return False
+def is_sentence(sentence):
+    return isinstance(sentence, Sentence) or isinstance(sentence, bool)
