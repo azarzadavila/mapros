71e8e30d2074a1eb68d28034607c7ebbf1a63307
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-10-29 15:46:50 +0100

    Refactor proof

--- formal/proof.py
+++ formal/proof.py
@@ -1,5 +1,4 @@
-from formal.grammar import IDSentence
-from formal.rules_inference import MAP_RULE, StatementProof
+from formal.rules_inference import MAP_RULE, SentenceProof
 
 # TODO internal current node
 # TODO first element without proof
@@ -35,30 +34,30 @@ def check_scope(position, reference):
     return False
 
 
-def change_add_tuple(position, statement_proof):
+def change_add_tuple(position, sentence_proof):
     """
-    Consider a shift to the positions in sentences at statement_proof from position
-    For example, if position is (2,3) and statement proof has a sentence (2,3,1) then that sentence will be changed to
+    Consider a shift to the positions in proofs at sentence_proof from position
+    For example, if position is (2,3) and sentence proof has a proof (2,3,1) then that sentence will be changed to
     (2,4,1)
     :param position: tuple
-    :param statement_proof: StatementProof instance
+    :param sentence_proof: SentenceProof instance
     """
-    if statement_proof is not None:
-        for i in range(len(statement_proof.sentences)):
-            if isinstance(statement_proof.sentences[i], tuple):
-                if statement_proof.sentences[i][: len(position)] == position:
-                    statement_proof.sentences[i] = (
+    if sentence_proof is not None:
+        for i in range(len(sentence_proof.proofs)):
+            if isinstance(sentence_proof.proofs[i], tuple):
+                if sentence_proof.proofs[i][: len(position)] == position:
+                    sentence_proof.proofs[i] = (
                         position[:-1]
                         + (position[-1] + 1,)
-                        + statement_proof.sentences[len(position) :]
+                        + sentence_proof.proofs[len(position) :]
                     )
 
 
 class Proof:
-    def __init__(self, statement, children, statement_proof):
-        self.statement = statement
+    def __init__(self, sentence, children, sentence_proof):
+        self.sentence = sentence
         self.children = children
-        self.statement_proof = statement_proof
+        self.sentence_proof = sentence_proof
 
     def get_proof(self, position):
         """
@@ -74,111 +73,151 @@ class Proof:
                 raise ValueError("Incorrect position : {}".format(position))
         return current
 
-    def add_statement_child(self, statement, position):
+    def add_sentence_child(self, sentence, position):
         """
-        Adds a new statement, by creating a child proof to the parent at the given position
-        :param statement:
+        Adds a new sentence, by creating a child proof to the parent at the given position
+        :param sentence:
         :param position: tuple indicating the position of the parent
         """
         parent = self.get_proof(position)
-        parent.children.append(Proof(statement, [], None))
+        parent.children.append(Proof(sentence, [], None))
 
     def change_position_child(self, position):
         """
-        Changes the StatementProof that need change due to the addition of a new proof at position.
+        Changes the SentenceProof that need change due to the addition of a new proof at position.
         This method is recursive and will be applied to each child
         :param position: tuple
         """
-        change_add_tuple(position, self.statement_proof)
+        change_add_tuple(position, self.sentence_proof)
         for child in self.children:
             child.change_position_child(position)
 
     def change_position_parent(self, position):
         """
-        Changes the StatementProof that need change due to the addition of a new proof at position.
+        Changes the SentenceProof that need change due to the addition of a new proof at position.
         This method should be called from the parent from which a new child proof was added
         :param position: tuple
         """
-        change_add_tuple(position, self.statement_proof)
+        change_add_tuple(position, self.sentence_proof)
         for child in self.children[position[-1] + 1 : len(self.children)]:
             child.change_position_child(position)
 
-    def add_statement(self, statement, position):
+    def add_sentence(self, sentence, position):
         """
-        Adds a new statement at the given position if such parent exists
-        :param statement:
+        Adds a new sentence at the given position if such parent exists
+        :param sentence:
         :param position: tuple indicating the position of the proof
         """
         parent = self.get_proof(position[:-1])
         if position[-1] > len(parent.children):
             raise ValueError("Incorrect position to add a proof : {}".format(position))
-        parent.children.insert(position[-1], Proof(statement, [], None))
+        parent.children.insert(position[-1], Proof(sentence, [], None))
         parent.change_position(position)
 
-    def add_statement_rule(self, position, rule, *args):
+    def apply_rule(self, position, rule, proofs, args):
+        """
+        Applies the given rule if it had to be considered to add a proof at position
+        :param position:
+        :param rule:
+        :param proofs:
+        :param args:
+        :return: the generated sentence or None if the could not be applied
+        """
+        try:
+            parent = self.get_proof(position[:-1])
+            if position[-1] > len(parent.children):
+                raise ValueError(
+                    "incorrect position to apply a rule : {}".format(position)
+                )
+            proofs = list(proofs)
+            for proof in proofs:
+                if isinstance(proof, tuple):
+                    if not check_scope(proof, position):
+                        raise ValueError(
+                            "incorrect scope : {} in {}".format(proof, position)
+                        )
+            if rule == "premise":
+                return True
+            elif rule == "hypothesis":
+                return position[-1] == 0
+            elif rule == "reductio_ad_absurdum":
+                hypothesis = self.get_proof(proofs[0])
+                if not hypothesis.sentence_proof == "hypothesis":
+                    raise ValueError("The passed sentence is not an hypothesis")
+            elif rule == "deduction_theorem":
+                pass
+            elif rule == "universal_generalization":
+                pass
+            elif rule == "universal_instantiation":
+                pass
+            elif rule == "existential_generalization":
+                pass
+            elif rule == "existential_instantiation":
+                pass
+            rule_fct = MAP_RULE[rule]
+            sentence = rule_fct(*map(self.get_sentence, proofs), *args)
+            return sentence
+        except (ValueError, TypeError):
+            return None
+
+    def add_sentence_rule(self, position, rule, proofs, args):
         """
         Adds a new proof at the given position if such parent exists by using an inference rule
         :param position: tuple indicating the position of the proof
         :param rule: string indicating the inference rule to use
-        :param args: sentences to use for the rule, either as external sentences,
-                     tuple indicating a position in this proof or directly a sentence
+        :param proofs: tuple indicating positions for the proofs to use
+        :param args: additional args required by the rule (e.g. a variable to rename)
         """
         parent = self.get_proof(position[:-1])
-        if position[-1] > len(parent.children):
-            raise ValueError("Incorrect position to add a proof : {}".format(position))
-        args = list(args)
-        rule_fct = MAP_RULE[rule]
-        statement = rule_fct(*map(self.get_statement, args))
-        statement_proof = StatementProof(rule, args)
-        parent.children.insert(position[-1], Proof(statement, [], statement_proof))
+        sentence = self.apply_rule(position, rule, proofs, args)
+        if sentence is None:
+            raise ValueError("incorrect rule application")
+        sentence_proof = SentenceProof(rule, proofs, args)
+        parent.children.insert(position[-1], Proof(sentence, [], sentence_proof))
         parent.change_position(position)
 
-    def edit_statement(self, statement, position):
+    def edit_sentence(self, sentence, position):
         """
-        Edit a statement
-        :param statement: new statement to replace the previous one
+        Edit a sentence
+        :param sentence: new sentence to replace the previous one
         :param position: tuple indicating the position of the proof
         """
         proof = self.get_proof(position)
-        proof.statement = statement
+        proof.sentence = sentence
 
-    def edit_statement_proof(self, statement_proof, position):
+    def edit_sentence_proof(self, sentence_proof, position):
         """
-        Edit the statement proof of a proof
-        :param statement_proof: StatementProof instance
+        Edit the sentence proof of a proof
+        :param sentence_proof: SentenceProof instance
         :param position: tuple indicating the position of the proof
         """
         proof = self.get_proof(position)
-        proof.statement_proof = statement_proof
+        proof.sentence_proof = sentence_proof
 
-    def get_statement(self, arg):
+    def get_sentence(self, arg):
         """
-        Returns the statement associated with arg
-        :param arg: either something that passes check_sentence or a tuple indicating a position
-        :return: statement
+        Returns the sentence associated with arg
+        :param arg: a tuple indicating a position
+        :return: sentence
         """
         if isinstance(arg, tuple):
             proof = self.get_proof(arg)
-            return proof.statement
-        if isinstance(arg, IDSentence):
-            # TODO retrieve the sentence from an IDSentence
-            pass
+            return proof.sentence
+        # if isinstance(arg, IDSentence):
+        # TODO retrieve the sentence from an IDSentence
+        # pass
         raise ValueError("Arg is neither a tuple nor a IDSentence")
 
-    def add_statement_with_rule_child(self, position, rule, *args):
+    def add_sentence_with_rule_child(self, position, rule, proofs, args):
         """
         Adds a new proof as a child of the given position by using an inference rule
         :param position: parent position
         :param rule: string indicating the inference rule to use
-        :param args: sentences to use for the rule, either as external sentences,
-                     tuple indicating a position in this proof or directly a sentence
+        :param proofs: proofs to use
+        :param args: additional args required by the rule (e.g. a variable to rename)
         """
         parent = self.get_proof(position)
-        args = list(args)
-        rule_fct = MAP_RULE[rule]
-        statement = rule_fct(*map(self.get_statement, args))
-        statement_proof = StatementProof(rule, args)
-        parent.children.append(Proof(statement, [], statement_proof))
+        self.add_sentence_rule(position + (len(parent.children),), rule, proofs, args)
 
     def check_proof(self, position):
         """
@@ -187,29 +226,17 @@ class Proof:
         :return: true if the proof is correct, false otherwise
         """
         proof = self.get_proof(position)
-        if proof.statement_proof is None:
+        if proof.sentence_proof is None:
             return False
-        sentences = proof.statement_proof.sentences
-        if proof.statement_proof.rule == "premise":
-            return True
-        if proof.statement_proof.rule == "hypothesis":
-            return position[-1] == 0
-        if proof.statement_proof.rule not in MAP_RULE:
+        sentence = self.apply_rule(
+            position,
+            proof.sentence_proof.rule,
+            proof.sentence_proof.proofs,
+            proof.sentence_proof.args,
+        )
+        if sentence is None:
             return False
-        rule_fct = MAP_RULE[proof.statement_proof.rule]
-        for sentence in sentences:
-            if isinstance(sentence, tuple):
-                if not check_scope(sentence, position):
-                    return False
-        try:
-            if proof.statement_proof.rule == "reductio_ad_absurdum":
-                hypothesis = self.get_proof(sentences[0])
-                if not hypothesis.statement_proof == "hypothesis":
-                    raise ValueError("The passed sentence is not an hypothesis")
-            statement = rule_fct(*map(self.get_statement, sentences))
-        except (ValueError, TypeError):
-            return False
-        if not statement == proof.statement:
+        if not proof.sentence == sentence:
             return False
         for i in range(len(proof.children)):
             if not self.check_proof(position + (i,)):
