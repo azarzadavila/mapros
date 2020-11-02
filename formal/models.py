876ae9da4c9fcda8836a5f9e97b3e2f06374e785
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-11-02 09:03:41 +0100

    Init ProofModel

--- formal/models.py
+++ formal/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class ProofModel(models.Model):
+    statement = models.TextField()
+    statement_proof = (
+        models.TextField()
+    )  # TODO maybe a CharField with restricted size is better
+    parent = models.ForeignKey("self", blank=True, on_delete=models.CASCADE)
