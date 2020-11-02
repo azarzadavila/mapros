de9b97941fe6869a53777a55bb6b1eedd7050394
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-11-02 09:29:20 +0100

    Rename statement to sentence ProofModel

--- formal/models.py
+++ formal/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class ProofModel(models.Model):
-    statement = models.TextField()
-    statement_proof = (
+    sentence = models.TextField()
+    sentence_proof = (
         models.TextField()
     )  # TODO maybe a CharField with restricted size is better
     parent = models.ForeignKey("self", blank=True, on_delete=models.CASCADE)
