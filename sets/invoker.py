767ba947fb9d4bf18a3f4010677cd54762213553
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-20 14:51:03 +0100

    Invoker

---
+++ sets/invoker.py
@@ -0,0 +1,10 @@
+class Invoker:
+    def __init__(self):
+        self._commands = []
+
+    def store_command(self, command):
+        self._commands.append(command)
+
+    def execute_commands(self):
+        for command in self._commands:
+            command.execute()
