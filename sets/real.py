5fc4419b78c11ed024894cdf32a7dd343bf02980
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-20 15:35:41 +0100

    Apply order

--- sets/real.py
+++ sets/real.py
@@ -1,3 +1,6 @@
+from sets.utils import Order
+
+
 class Real:
     def __init__(self, symbol=None):
         self.lt = set()
@@ -21,4 +24,20 @@ class Real:
 
 
 def apply_order(order, v1, v2):
-    pass
+    if order == Order.LT:
+        v1.lt.add(v2)
+        v2.gt.add(v1)
+    elif order == Order.LE:
+        v1.le.add(v2)
+        v2.ge.add(v1)
+    elif order == Order.EQ:
+        v1.eq.add(v2)
+        v2.eq.add(v1)
+    elif order == Order.GT:
+        v1.gt.add(v1)
+        v2.lt.add(v2)
+    elif order == Order.GE:
+        v1.ge.add(v2)
+        v2.le.add(v1)
+    else:
+        raise ValueError("Unknown order : {}".format(order))
