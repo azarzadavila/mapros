066f8f9a245009e4bff9a3c50fa53876826e2c38
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-12-22 17:32:07 +0100

    unexpected input

--- sets/views.py
+++ sets/views.py
@@ -3,7 +3,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+from lark import UnexpectedInput
+
 from sets.serializers import AskSerializer
+from sets.proof import Proof
+from sets.parser import parse
+from sets.invoker import Invoker
 
 
 class Ask(APIView):
@@ -12,5 +17,16 @@ class Ask(APIView):
     def post(self, request, format=None):
         serializer = AskSerializer(data=request.data)
         if serializer.is_valid():
+            invoker = Invoker()
+            proof = Proof()
+            for s in serializer.validated_data["premises"]:
+                try:
+                    command = parse(s, proof)
+                except UnexpectedInput:
+                    return Response(
+                        {"message": "unknown command for premise : {}".format(s)}
+                    )
+                invoker.store_command(command)
+            invoker.execute_commands()
             return Response(status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
