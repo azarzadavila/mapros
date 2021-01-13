5022ac17b1830258595c2f67b0c686548dc96c85
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2021-01-13 15:49:40 +0100

    update

--- leanstart/lean2.py
+++ leanstart/lean2.py
@@ -5,6 +5,7 @@ theorem: "theorem" LETTER_LIKE hypotheses ":" result
 hypotheses: _hypothesis*
 _hypothesis: "(" hypothesis ")" | "{" hypothesis "}"
 hypothesis: function_declaration | declaration | named_hypothesis | basic
+function_declaration: LETTER_LIKE ":" DOMAIN "→" DOMAIN
 declaration: LETTER_LIKE* ":" DOMAIN
 DOMAIN: LETTER_LIKE
 named_hypothesis: LETTER_LIKE ":" LETTER_LIKE
@@ -16,7 +17,7 @@ LETTER_LIKE: /[^\s\n\t\r:\(\)\[\]]+/
 """
 
 parser = Lark(lean, start="theorem")
-s1 = "theorem mean (a b) (c : d) (a b : R) : x+a=d+1"
+s1 = "theorem mean (a b) (c : d) (a b : R) (f: A → B) : x+a=d+1"
 x = parser.parse(s1)
 for child in x.children[1].children:
     print(child)
