37f7bd4e8b10c1097c0722e18a5ac62294ad399a
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2021-01-13 10:39:34 +0100

    update

--- leanstart/lean2.py
+++ leanstart/lean2.py
@@ -3,7 +3,9 @@ from lark import Lark, Transformer
 lean = r"""
 theorem: "theorem" LETTER_LIKE _hypothesis* ":" result
 _hypothesis: "(" hypothesis ")" | "{" hypothesis "}"
-hypothesis: LETTER_LIKE*
+hypothesis: declaration | basic
+declaration: LETTER_LIKE ":" LETTER_LIKE
+basic: LETTER_LIKE*
 result: LETTER_LIKE*
 LETTER_LIKE: /[^\s\n\t\r:\(\)\[\]]+/
 %import common.WS
@@ -11,5 +13,5 @@ LETTER_LIKE: /[^\s\n\t\r:\(\)\[\]]+/
 """
 
 parser = Lark(lean, start="theorem")
-s1 = "theorem mean (a b) (c d) : x+a=d+1"
+s1 = "theorem mean (a b) (c : d) : x+a=d+1"
 print(parser.parse(s1))
