e94f9b1c3bc7e3c58a8b8e1d809be4559cb7bd0b
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2021-02-04 11:54:54 +0100

    update tokenizer

--- leanstart/tokenizer.py
+++ leanstart/tokenizer.py
@@ -2,9 +2,14 @@ from lark import Lark
 
 grammar = r"""
     start: _SEP* token (_SEP+ token)* _SEP*
-    token: symbol | command | other
-    symbol: /have/
+    token: symbol | command | ident | other
+    symbol: /have|intro|use/
     command: /import|open|theorem/
+    ident: _atomic_ident
+    _atomic_ident: _atomic_ident_start _atomic_ident_rest*
+    _atomic_ident_start: LETTER_LIKE
+    LETTER_LIKE: /[a-zA-Z]/
+    _atomic_ident_rest: _atomic_ident_start | /[0-9]/
     //other should actually tell not to match symbols and commands
     other: /\S+/
     //token: symbol | command | ident | string | char | numeral | decimal | quoted_symbol | doc_comment | mod_doc_comment
