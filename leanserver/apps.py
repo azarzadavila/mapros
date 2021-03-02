38660b5e152e1173a7b7deddd25b9cf39ce3a385
azarzadavila <andres.zarzadavila@student.uclouvain.be>
2021-03-02 14:31:39 +0100

    start leanserver

---
+++ leanserver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LeanserverConfig(AppConfig):
+    name = 'leanserver'
--- mapros/settings.py
+++ mapros/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "authentification.apps.AuthentificationConfig",
     "corsheaders",
     "formal",
+    "leanserver",
 ]
 
 MIDDLEWARE = [
